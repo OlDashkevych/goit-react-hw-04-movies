9eecd874ffa39868df71d4dd563683d317e91a6e
OlDashkevych <dashkevich672@gmail.com>
2020-01-18 22:24:41 +0200

    Updates

---
+++ precache-manifest.6e5829209f726647e974cf0cf2687d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b569dc28f5c8de1c1095e5a4e7c8aeba",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "d037d65b54e09f8dc158",
+    "url": "/goit-react-hw-04-movies/static/css/main.cfe37169.chunk.css"
+  },
+  {
+    "revision": "efd19af292a3fbe4a205",
+    "url": "/goit-react-hw-04-movies/static/js/2.cafb91c2.chunk.js"
+  },
+  {
+    "revision": "d037d65b54e09f8dc158",
+    "url": "/goit-react-hw-04-movies/static/js/main.6f0009f3.chunk.js"
+  },
+  {
+    "revision": "faff8ac03dfaf1fc4d90",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.3ab1a206.js"
+  }
+]);
